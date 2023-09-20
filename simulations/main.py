8285133a8a4b6f75a34fa0f73b0031c3aa4a60d3
anahar1 <anahar1@asu.edu>
2023-09-20 14:45:21 -0700

    update exp main

--- simulations/main.py
+++ simulations/main.py
@@ -1,4 +1,5 @@
 import uuid
+import numpy as np
 
 """
 Authority Class
@@ -6,10 +7,11 @@ Authority Class
 class Authority:
 
     def __init__(self):
-        self.punishment = None
-        self.tolerance = None
-        self.severity = None
+        self.pi = None
+        self.tau = None
+        self.s = None
         self.alpha = None
+        self.v = None
     
     def utility_authority(a, alpha, pi, tau, s):
         """
@@ -47,6 +49,15 @@ class Authority:
         else:
             return 0
 
+    def probability(v, a_i):
+        """
+        Calculates the probability of a punishment being enacted.
+        """
+        return v + (1 - v) * a_i
+
+    def view():
+
+        pass
 
     def opt_tolerance():
         """
@@ -68,11 +79,10 @@ class Individual:
 
     def __init__(self):
         self.id = uuid.uuid4()
-        self.action = None
-        self.beta = None
-        self.desire = None
+        self.b_i = None
+        self.d_i = None
 
-    def opt_action(d_i, b_i, pi, tau, s):
+    def opt_action(d_i, b_i, pi, tau, s, v):
         """
         Calculates individual i's optimal action as a function of known parameters.
 
@@ -82,7 +92,27 @@ class Individual:
         :param tau: a tolerance in [0, 1]
         :param s: a severity parameter > 0
         """
-        pass
+
+        if pi == "constant":
+            if d_i <= tau or (b_i > (1 - v) and  d_i > (b_i * tau + v * s)/(b_i - (1 - v) * s)):
+                return d_i
+            else:
+                return tau
+        else:
+            if d_i <= tau or (v == 1 and s <= b_i) or (v < 1 and Individual.a_peak(tau, v, s, b_i) >= d_i):
+                return d_i
+            if d_i > tau and ((v == 1 and s > b_i) or (v < 1 and Individual.a_peak(tau, v, s, b_i) <= tau)):
+                return tau
+            else:
+                return Individual.a_peak(tau, v, s, b_i)
+
+    def a_peak(tau, v, s, b_i):
+        """
+        Calculates individual's peak utility as a function of known parameters.
+        """
+
+        return 0.5 (tau - (v * s - b_i) / (s * (1 - v)))
+    
 
     def utility_individual(b_i, a_i, d_i, p_i):
         """
@@ -97,19 +127,61 @@ class Individual:
         return b_i (1 - d_i + a_i) - p_i
 
 
+def experiment(individuals, num_individuals, authority,  num_rounds):
+    actions_history = []
+
+    # Step 1: All individuals calculate and enact a_i.
+
+    # Step 2: The authority calculates and enacts punishments.
+
+    # Step 3: Individuals update one of (d_i, beta_i) according to an update rule.
+
+    # Step 4: The authority updates one of (tau, s) for the next round.
+
+    # Measurement:
+    # Utilities (authority and individuals).
+    # All parameters.
+    # Individuals' actions. (Imagine a plot with time vs. everyone's action trajectories).
+    
+    for round in range(num_rounds):
+        # Step 1
+        actions = [individuals[i].opt_action(individuals[i].d_i, individuals[i].b_i, authority.pi, authority.tau, authority.s, authority.v) for i in range(num_individuals)]
+        
+        actions_history.append(actions)
+
+
+        # Step 2
+
+        # Step 3
+
+        # Step 4
+
+
 if __name__ == "__main__":
-    for round in range(100):
-        # Step 1: All individuals calculate and enact a_i.
+    # Setup.
 
-        # Step 2: The authority calculates and enacts punishments.
+    # Total amount of indivduals.
+    num_individuals = 10000 
 
-        # Step 3: Individuals update one of (d_i, beta_i) according to an update rule.
+    # Generate evenly spaced desires for a large number of individuals
+    desires = np.linspace(0, 1, num_individuals) 
 
-        # Step 4: The authority updates one of (tau, s) for the next round.
+    # Create an array of Individual objects with evenly spaced desires
+    individuals = [Individual() for _ in range(num_individuals)]
+    
+    for i in range(num_individuals):
+        individuals[i].d_i = desires[i]
+        individuals[i].b_i = np.random.uniform(0, 10)
 
-        # Measurement:
-        # Utilities (authority and individuals).
-        # All parameters.
-        # Individuals' actions. (Imagine a plot with time vs. everyone's action trajectories).
-        # Total (or normalized) amount of dissent.
-        pass
+    # Create an Authority object with random values
+    authority = Authority()
+    authority.tau = np.random.rand()
+    authority.s = np.random.uniform(0, 10)
+    authority.alpha = np.random.uniform(0, 10)
+    authority.v = np.random.rand()
+    authority.pi = "constant"
+
+    # Run experiment
+    experiment(individuals, num_individuals, authority, 100)
+
+    exit(0)
