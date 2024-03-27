5e59473484e6f75277ddd573d03bfd31d71dba93
Josh Daymude <jdaymude@asu.edu>
2024-03-26 21:34:18 -0700

    Parallelized the first network simulation experiment

--- code/experiments.py
+++ code/experiments.py
@@ -3,38 +3,70 @@
 # Authors:  Joshua J. Daymude (jdaymude@asu.edu).
 
 """
-experiments: TODO
+experiments: Network simulation experiments for the Censorship-Dissent model.
 """
 
 from engine import engine
 from helper import dump_np
 
 import argparse
+from itertools import repeat
 import numpy as np
 import os.path as osp
-from tqdm import tqdm
 from tqdm.contrib.concurrent import process_map
+
+
+def anish_thesis_worker(idx, T, N, R, pis, psis, mu_deltas, nus, seeds):
+    """
+    Parallel worker function for anish_thesis.
+
+    :param idx: a tuple of int indices specifying the parameters for this run
+    :param T: an int number of trials to run for this set of parameters
+    :param N: an int number of individuals
+    :param R: an int number of rounds to run per trial
+    :param pis: a list of string punishment functions
+    :param psis: a list of the authority's float punishment severities (> 0)
+    :param mu_deltas: a list of individuals' float mean desired dissents (in [0,1])
+    :param nu: a list of the authority's float surveillances (in [0,1])
+    :param seeds: a list of int seeds for random number generation
+
+    :returns: a tuple (idx, deltafs, actfs):
+        - idx: the tuple of int indices given as input
+        - deltafs: a TxN array of final desired dissents for each trial/individual
+        - actfs: a TxN array of final actions for each trial/individual
+    """
+    # Unpack parameters.
+    pi = pis[idx[0]]
+    psi = psis[idx[1]]
+    mu_delta = mu_deltas[idx[2]]
+    nu = nus[idx[3]]
+
+    deltafs, actfs = np.zeros((T, N)), np.zeros((T, N))
+
+    for t in range(T):
+        rng = np.random.default_rng(seeds[t])
+        deltas = np.minimum(np.maximum(rng.normal(mu_delta, 0.1, N), 0), 1)
+        _, delta_hist, act_hist = engine(
+            N=N, R=R, rule='d2a', w=0.5, deg=3, ptri=0.25, deltas=deltas,
+            betas=np.repeat(1, N), nu=nu, pi=pi, tau=0.25, sigma_tau=0.05,
+            psi=psi, sigma_psi=0.05, seed=seeds[t])
+        deltafs[t], actfs[t] = delta_hist[:,R-1], act_hist[:,R-1]
+
+    return (idx, deltafs, actfs)
 
 
 def anish_thesis(seed=None, num_cores=1):
     """
-    TODO
+    An experiment investigating individuals' convergence mean desired dissents
+    and actions across a variety of parameter sweeps.
+
+    :param seed: an int seed for random number generation
+    :param num_cores: an int number of processors to parallelize over
     """
-    # Fixed parameters.
-    # T = 10
-    T = 2
-    # N = 250
-    N = 25
+    # Numbers of trials, individuals, and rounds.
+    T = 10
+    N = 250
     R = 100
-    rule = 'd2a'
-    w = 0.5
-    # deg = 5
-    deg = 3
-    ptri = 0.25
-    betas = np.repeat(1, N)
-    tau = 0.25
-    sigma_tau = 0.05
-    sigma_psi = 0.05
 
     # Sweep parameters.
     pis = ['constant', 'linear']
@@ -50,16 +82,14 @@ def anish_thesis(seed=None, num_cores=1):
     # Set up random seeds for each trial.
     seeds = np.random.default_rng(seed).integers(0, 2**32, size=T)
 
-    # Perform the experiment.
-    for i, j, k, l in tqdm(np.ndindex(len(pis), len(psis), len(mu_deltas), len(nus))):
-        pi, psi, mu_delta, nu = pis[i], psis[j], mu_deltas[k], nus[l]
-        for t in range(T):
-            rng = np.random.default_rng(seeds[t])
-            deltas = np.minimum(np.maximum(rng.normal(mu_delta, 0.1, N), 0), 1)
-            _, delta_hist, act_hist = engine(N, R, rule, w, deg, ptri, deltas,
-                                             betas, nu, pi, tau, sigma_tau, psi, sigma_psi, seeds[t])
-            deltaf_all[i, j, k, l, t] = delta_hist[:,R-1]
-            actf_all[i, j, k, l, t] = act_hist[:,R-1]
+    # Parallelize the experiment sweep and trials.
+    idxs = list(np.ndindex(len(pis), len(psis), len(mu_deltas), len(nus)))
+    p = process_map(anish_thesis_worker, idxs, repeat(T), repeat(N), repeat(R),
+                    repeat(pis), repeat(psis), repeat(mu_deltas), repeat(nus),
+                    repeat(seeds), max_workers=num_cores)
+    for idx, deltafs, actfs in p:
+        deltaf_all[idx] = deltafs
+        actf_all[idx] = actfs
 
     # Save results.
     dump_np(osp.join('results', 'anish_thesis_deltas.npy'), deltaf_all)
