cf0a1a42947141c8099abcaee7449229b5264c8d
Josh Daymude <jdaymude@asu.edu>
2024-05-30 17:09:55 -0700

    Transposed the phase diagram figure for easier interpretation, plus minor label updates

--- code/opt_action.py
+++ code/opt_action.py
@@ -3,7 +3,7 @@
 # Authors:  Joshua J. Daymude (jdaymude@asu.edu).
 
 """
-opt_action: Calculates and plots an individual's optimal action dissent and
+opt_action: Calculates and plots an individual's optimal action and
 corresponding utility as a function of their own desired dissent and boldness
 and noisy estimates of the authority's tolerance and severity.
 """
@@ -13,11 +13,39 @@ import numpy as np
 import os.path as osp
 
 
+def dcon(beta, nu, tau, psi):
+    """
+    Computes the critical point between (full) self-censorship and defiance
+    under constant punishment.
+
+    :param beta: the individual's float boldness (> 0)
+    :param nu: the authority's float surveillance (in [0,1])
+    :param tau: the authority's float tolerance (in [0,1])
+    :param psi: the authority's float punishment severity (> 0)
+    :returns: the float critical point for constant punishment
+    """
+    return (beta * tau + nu * psi) / (beta - (1 - nu) * psi)
+
+
+def dlin(beta, nu, tau, psi):
+    """
+    Computes the critical point between self-censorship and defiance as well as
+    the level of self-censorship enacted under linear punishment.
+
+    :param beta: the individual's float boldness (> 0)
+    :param nu: the authority's float surveillance (in [0,1])
+    :param tau: the authority's float tolerance (in [0,1])
+    :param psi: the authority's float punishment severity (> 0)
+    :returns: the float critical point for linear punishment
+    """
+    return (tau + (beta - nu * psi) / ((1 - nu) * psi)) / 2 if nu < 1 else 0
+
+
 def opt_action(delta, beta, nu, pi, tau, psi):
     """
-    Computes an individual's optimal action dissent as a function of their
-    parameters, the authority's parameters, and their noisy estimates of the
-    authority's tolerance and severity.
+    Computes an individual's optimal action as a function of their parameters,
+    the authority's parameters, and their noisy estimates of the authority's
+    tolerance and severity.
 
     :param delta: the individual's float desire to dissent (in [0,1])
     :param beta: the individual's float boldness (> 0)
@@ -25,32 +53,32 @@ def opt_action(delta, beta, nu, pi, tau, psi):
     :param pi: 'constant' or 'linear' punishment
     :param tau: the authority's float tolerance (in [0,1])
     :param psi: the authority's float punishment severity (> 0)
-    :returns: the individual's float optimal action dissent (in [0,delta])
+    :returns: the individual's float optimal action (in [0,delta])
     """
     assert pi in ['constant', 'linear'], f'ERROR: Unrecognized punishment function \"{pi}\"'
 
     if pi == 'constant':
         # Compliant/defiant individuals act = desire; the rest self-censor.
-        dcon = (beta * tau + nu * psi) / (beta - (1 - nu) * psi)
-        if delta <= tau or (beta > (1 - nu) * psi and delta > dcon):
+        d = dcon(beta, nu, tau, psi)
+        if delta <= tau or (beta > (1 - nu) * psi and delta > d):
             return delta
         else:
             return tau
     elif pi == 'linear':
         # If the authority's surveillance is perfect, we simply compare the
         # individual's boldness to the authority's severity.
-        dlin = (tau + (beta - nu * psi) / ((1 - nu) * psi)) / 2 if nu < 1 else 0
-        if delta <= tau or (nu == 1 and beta >= psi) or (nu < 1 and delta <= dlin):
+        d = dlin(beta, nu, tau, psi)
+        if delta <= tau or (nu == 1 and beta >= psi) or (nu < 1 and delta <= d):
             return delta
-        elif (nu == 1 and beta < psi) or (nu < 1 and tau >= dlin):
+        elif (nu == 1 and beta < psi) or (nu < 1 and tau >= d):
             return tau
         else:
-            return dlin
+            return d
 
 
 def plot_opt_action(ax, beta, nu, pi, tau, psi, color='tab:blue'):
     """
-    Plot an individual's optimal action dissent vs. their desired dissent.
+    Plot an individual's optimal action vs. their desired dissent.
 
     :param ax: a matplotlib.Axes object to plot on
     :param beta: the individual's float boldness (> 0)
@@ -60,7 +88,7 @@ def plot_opt_action(ax, beta, nu, pi, tau, psi, color='tab:blue'):
     :param psi: the authority's float punishment severity (> 0)
     :param color: any color recognized by matplotlib
     """
-    # Evaluate the individual's optimal action dissent over a range of desires.
+    # Evaluate the individual's optimal action over a range of desires.
     deltas = np.linspace(0, 1, 10000)
     acts = np.array([opt_action(delta, beta, nu, pi, tau, psi) for delta in deltas])
     ax.plot(deltas, acts, c=color)
@@ -73,16 +101,16 @@ def plot_opt_action(ax, beta, nu, pi, tau, psi, color='tab:blue'):
                    yticklabels=['0', r'$\hat{\tau}_{i,r}$', '1'])
         else:
             # Visualize the discontinuity where defiance kicks in.
-            dcon = (beta * tau + nu * psi) / (beta - (1 - nu) * psi)
-            ax.scatter([dcon, dcon], [tau, dcon], c=[color, 'white'],
-                       edgecolor=color, zorder=3)
-            ax.set(xticks=[0, tau, dcon, 1], yticks=[0, tau, dcon, 1],
+            d = dcon(beta, nu, tau, psi)
+            ax.scatter([d, d], [tau, d], c=[color, 'white'], edgecolor=color,
+                       zorder=3)
+            ax.set(xticks=[0, tau, d, 1], yticks=[0, tau, d, 1],
                    xticklabels=['0', r'$\hat{\tau}_{i,r}$', r'$d_{i,r}^{con}$', '1'],
                    yticklabels=['0', r'$\hat{\tau}_{i,r}$', r'$d_{i,r}^{con}$', '1'])
     elif pi == 'linear':
-        dlin = (tau + (beta - nu * psi) / ((1 - nu) * psi)) / 2 if nu < 1 else 0
-        if nu < 1 and dlin > tau:
-            ax.set(xticks=[0, tau, dlin, 1], yticks=[0, tau, dlin, 1],
+        d = dlin(beta, nu, tau, psi)
+        if nu < 1 and d > tau:
+            ax.set(xticks=[0, tau, d, 1], yticks=[0, tau, d, 1],
                    xticklabels=['0', r'$\hat{\tau}_{i,r}$', r'$d_{i,r}^{lin}$', '1'],
                    yticklabels=['0', r'$\hat{\tau}_{i,r}$', r'$d_{i,r}^{lin}$', '1'])
         else:
@@ -99,17 +127,17 @@ if __name__ == "__main__":
     fig, ax = plt.subplots(2, 1, figsize=(4, 7.5), dpi=300, facecolor='white',
                            tight_layout=True)
 
-    # Plot optimal action dissent and utility when defiance exists.
+    # Plot optimal action and utility when defiance exists.
     plot_opt_action(ax[0], beta=1, nu=0.2, pi='constant', tau=0.25, psi=0.6)
     ax[0].set_title(r'(A) $\beta_i > (1 - \nu) \cdot \hat{\psi}_{i,r}$',
                     weight='bold')
-    ax[0].set(ylabel=r'Optimal Action Dissent, $a_{i,r}^*$')
+    ax[0].set(ylabel=r'Optimal Action $a_{i,r}^*$')
 
-    # Plot optimal action dissent and utility when defiance does not exist.
+    # Plot optimal action and utility when defiance does not exist.
     plot_opt_action(ax[1], beta=1, nu=0.2, pi='constant', tau=0.25, psi=2)
     ax[1].set_title(r'(B) otherwise', weight='bold')
-    ax[1].set(xlabel=r'Desire to Dissent, $\delta_i$',
-              ylabel=r'Optimal Action Dissent, $a_{i,r}^*$')
+    ax[1].set(xlabel=r'Desired Dissent $\delta_i$',
+              ylabel=r'Optimal Action $a_{i,r}^*$')
 
     fig.savefig(osp.join('..', 'figs', 'opt_action_constant.png'))
 
@@ -117,22 +145,22 @@ if __name__ == "__main__":
     fig, ax = plt.subplots(1, 3, figsize=(10.75, 4), dpi=300, facecolor='white',
                            tight_layout=True)
 
-    # Plot optimal action dissent and utility when v < 1 and defiance exists.
+    # Plot optimal action and utility when v < 1 and defiance exists.
     plot_opt_action(ax[0], beta=1, nu=0.2, pi='linear', tau=0.25, psi=1)
     ax[0].set_title(r'(A) $\nu < 1$ and $\hat{\tau}_{i,r} < d_{i,r}^{lin}$',
                     weight='bold')
-    ax[0].set(xlabel=r'Desire to Dissent, $\delta_i$',
-              ylabel=r'Optimal Action Dissent, $a_{i,r}^*$')
+    ax[0].set(xlabel=r'Desired Dissent $\delta_i$',
+              ylabel=r'Optimal Action $a_{i,r}^*$')
 
-    # Plot optimal action dissent and utility when v = 1 and beta >= s.
+    # Plot optimal action and utility when v = 1 and beta >= s.
     plot_opt_action(ax[1], beta=1, nu=1, pi='linear', tau=0.25, psi=0.6)
     ax[1].set_title(r'(B) $\nu = 1$ and $\beta_i \geq \hat{\psi}_{i,r}$',
                     weight='bold')
-    ax[1].set(xlabel=r'Desire to Dissent, $\delta_i$')
+    ax[1].set(xlabel=r'Desired Dissent $\delta_i$')
 
-    # Plot optimal action dissent and utility in the remaining case.
+    # Plot optimal action and utility in the remaining case.
     plot_opt_action(ax[2], beta=1, nu=1, pi='linear', tau=0.25, psi=2)
     ax[2].set_title(r'(C) otherwise', weight='bold')
-    ax[2].set(xlabel=r'Desire to Dissent, $\delta_i$')
+    ax[2].set(xlabel=r'Desired Dissent $\delta_i$')
 
     fig.savefig(osp.join('..', 'figs', 'opt_action_linear.png'))
--- code/phase_diagram.py
+++ code/phase_diagram.py
@@ -8,7 +8,7 @@ compliance, self-censorship, and defiance as a function of an individual's
 desire to dissent vs. the model's other parameters.
 """
 
-from opt_action import opt_action
+from opt_action import dcon, dlin, opt_action
 
 import matplotlib.pyplot as plt
 import numpy as np
@@ -31,18 +31,18 @@ def phase(delta, beta, nu, pi, tau, psi):
     if delta <= tau:  # Compliance.
         return 0
     elif opt_act >= tau and opt_act < delta:  # Self-censorship.
-        return 0.25 + 0.75 * ((opt_act - tau) / (delta - tau))
+        return 0.5 + 0.5 * ((opt_act - tau) / (delta - tau))
     else:  # Defiance.
         return 1
 
 
-def plot_phase_diagram(ax, yparam, beta, nu, pi, tau, psi):
+def plot_phase_diagram(ax, xparam, beta, nu, pi, tau, psi):
     """
     Plot a phase diagram of compliance, self-censorship, and defiance in the
     space of desires to dissent vs. the given parameter.
 
     :param ax: a matplotlib.Axes object to plot on
-    :param yparam: the y-axis parameter, from ['beta', 'nu', 'tau', 'psi']
+    :param xparam: the x-axis parameter, from ['beta', 'nu', 'tau', 'psi']
     :param beta: the individual's float boldness (> 0)
     :param nu: the authority's float surveillance (in [0,1])
     :param pi: 'constant' or 'linear' punishment
@@ -50,36 +50,48 @@ def plot_phase_diagram(ax, yparam, beta, nu, pi, tau, psi):
     :param psi: the authority's float punishment severity (> 0)
     """
     # Set an appropriate range for the y-axis parameter.
-    ylim = 3 if yparam in ['beta', 'psi'] else 1
+    xlim = 2.5 if xparam in ['beta', 'psi'] else 1
 
     # Create an array representing the 2D parameter space.
-    size = 1000
+    size = 1001  # Avoids divide-by-zero visual artifacts for dcon/dlin.
     phases = np.zeros((size, size))
+    xvals = np.linspace(0, xlim, size)
     deltas = np.linspace(0, 1, size)
-    yvals = np.linspace(0, ylim, size)
 
     # Compute the individual's optimal action dissent for each point in space,
     # then convert it to a compliant, self-censoring, or defiant phase.
-    for i, yval in enumerate(yvals):
-        for j, delta in enumerate(deltas):
-            if yparam == 'beta':
-                phases[i, j] = phase(delta, yval, nu, pi, tau, psi)
-            elif yparam == 'nu':
-                phases[i, j] = phase(delta, beta, yval, pi, tau, psi)
-            elif yparam == 'tau':
-                phases[i, j] = phase(delta, beta, nu, pi, yval, psi)
-            elif yparam == 'psi':
-                phases[i, j] = phase(delta, beta, nu, pi, tau, yval)
+    for i, delta in enumerate(deltas):
+        for j, xval in enumerate(xvals):
+            if xparam == 'beta':
+                phases[i, j] = phase(delta, xval, nu, pi, tau, psi)
+            elif xparam == 'nu':
+                phases[i, j] = phase(delta, beta, xval, pi, tau, psi)
+            elif xparam == 'tau':
+                phases[i, j] = phase(delta, beta, nu, pi, xval, psi)
+            elif xparam == 'psi':
+                phases[i, j] = phase(delta, beta, nu, pi, tau, xval)
             else:
-                assert(False, f'ERROR: Unrecognized y-parameter \'{yparam}\'')
+                assert False, f'ERROR: Unrecognized parameter \'{xparam}\''
 
     # Plot the phases as colors.
-    colors = [(0, 'orange'), (0.25, 'red'), (1, 'darkred')]
+    colors = [(0, 'orange'), (0.5, 'red'), (1, 'darkred')]
     cmap = plt.cm.colors.LinearSegmentedColormap.from_list('cphase', colors)
-    im = ax.pcolormesh(deltas, yvals, phases, cmap=cmap, shading='auto')
+    im = ax.pcolormesh(xvals, deltas, phases, cmap=cmap, shading='auto')
+
+    # Plot dcon or dlin, the critical points, over the heatmap.
+    dfun = dcon if pi == 'constant' else dlin
+    if xparam == 'beta':
+        ds = [dfun(xval, nu, tau, psi) for xval in xvals]
+    elif xparam == 'nu':
+        ds = [dfun(beta, xval, tau, psi) for xval in xvals]
+    elif xparam == 'tau':
+        ds = [dfun(beta, nu, xval, psi) for xval in xvals]
+    elif xparam == 'psi':
+        ds = [dfun(beta, nu, tau, xval) for xval in xvals]
+    ax.plot(xvals, ds, color='white')
 
     # Set axes information that is common across plots.
-    ax.set(xlim=[0, 1], ylim=[0, ylim])
+    ax.set(xlim=[0, xlim], ylim=[0, 1])
     ax.set_box_aspect(1)
 
     return im
@@ -103,19 +115,19 @@ if __name__ == "__main__":
         im = plot_phase_diagram(ax[1, i], param, beta=1, nu=0.5, pi='linear',
                                 tau=0.25, psi=1.5)
         # Set axes information.
-        ax[0, i].set_title(label, weight='bold')
-        ax[1, i].set(xlabel=r'Desire to Dissent, $\delta_i$')
+        ax[1, i].set_xlabel(label, weight='bold', fontsize='large')
         if i == 0:
             ax[0, i].set(ylabel=r'Constant Punishment $\pi$')
             ax[1, i].set(ylabel=r'Linear Punishment $\pi$')
-        if params[i] != 'tau':
-            ax[1, i].set_xticks([0, 0.25, 1])
-            ax[1, i].set_xticklabels(['0', r'$\tau$', '1'])
+        if param != 'tau':
+            ax[0, i].set(yticks=[0, 0.25, 1], yticklabels=['0', r'$\tau$', '1'])
+            ax[1, i].set(yticks=[0, 0.25, 1], yticklabels=['0', r'$\tau$', '1'])
+    fig.supylabel(r'Desired Dissent $\delta_i$')
 
     # Create and configure a colorbar shared by all axes.
-    cbar = fig.colorbar(im, ax=ax.ravel().tolist())
+    cbar = fig.colorbar(im, ax=ax, pad=0.02)
     cbar.set_label('')
-    cbar.set_ticks([0, 0.25, 1])
-    cbar.set_ticklabels(['Compliant', 'Self-Censoring', 'Defiant'])
+    cbar.set_ticks([0, 0.5, 1])
+    cbar.set_ticklabels(['Compliant', 'Fully Self-\nCensoring', 'Defiant'])
 
     fig.savefig(osp.join('..', 'figs', 'phase_diagram.png'))
