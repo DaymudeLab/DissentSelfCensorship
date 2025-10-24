447da708dce26845b7498b4b61c4183974a45e74
yhuan467 <166069934+yhuan467@users.noreply.github.com>
2025-10-24 10:12:32 -0700

    change no plot 2d

--- code/hillclimbing.py
+++ code/hillclimbing.py
@@ -212,7 +212,7 @@ def rmhc_trial(N, R, delta, beta, pi, tau0, psi0, nu0, alpha, eps, seed,k_mutate
             params[:, r] = params[:, r-1]
             
     #after getting all params, plot!
-    _plot_candidates_2d(cands_history)
+    #_plot_candidates_2d(cands_history)
 
     return params, pol_costs, pun_costs, deltas, betas
     
